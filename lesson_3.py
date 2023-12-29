d48b0201ebb6a4f35e512284c337896e097326b3
Vedat <vedatesk0@gmail.com>
2023-12-29 16:22:50 +0300

    exam

---
+++ lesson_3.py
@@ -0,0 +1,12 @@
+name1 = 'Ali'
+name2 = 'Veli'
+name3 = 'Salih'
+print(f"Sınıfın en akıllısı, {name1}\n"
+      f"Sınıfın en delisi, {name2}\n"
+      f"Sınıfın en yaraması, {name3}")
+
+name1 = 'Ali'
+name2 = 'Veli'
+name3 = 'Salih'
+print(name1, name2, name3, sep='**')
+
