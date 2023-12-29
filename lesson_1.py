3d90e0b203b9710d3d92feac7a19ed22732f17c9
Vedat <vedatesk0@gmail.com>
2023-12-29 15:13:58 +0300

    Phyton task 1

---
+++ lesson_1.py
@@ -0,0 +1,27 @@
+print("Hello World!")
+print("""
+      Ankara is central of Turkey.
+      Istanbul is trade center  of Turkey.
+      Bursa is the first central of the Ottoman Empire.""")
+
+print('My son\'s game was good.')
+
+print("We need to leave soon,\nIt's getting late.\nI need to eathing some.\n\t\tI'm getting hungry")
+name = input("Please enter your name: " )
+surname = input("Please enter surname: " )
+age =int(input("Please enter your age: " ))
+
+print(f"My name is {name}")
+print(f"My surname is {surname}")
+print(f"I am {age} years old.")
+
+if age >= 40:
+    print('Welcome')
+else:
+    print('you must go low down')
+
+ 
+
+      
+
+
