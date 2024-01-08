7730acd20dfb1cc05778bcf4aa241353714c2b95
Vedat <vedatesk0@gmail.com>
2024-01-08 23:49:33 +0300

    task py

---
+++ lesson_10.py
@@ -0,0 +1,29 @@
+# while
+
+number = 0
+while number < 6:
+    print(number)
+    number += 1
+
+i = 1
+while i < 6:
+    print('Semih')
+    if i == 3:
+        print('Ekrem')
+    i += 1
+
+i = 1
+while i < 6:
+    print(i)
+    if i == 3:
+      break
+    i += 1
+
+my_list = ["a", "b", "c", "d", "e", "f"]
+a = 0
+while a < len(my_list):
+    print("square of {} is {}".format(a, a**2))
+    a += 1
+
+listem = range(0,10)
+print(listem)
